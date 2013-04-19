daa6dbc09ded8f7b3adb758fe3e9e9408f302ed8
Casey Olson <casey@bookcasey.com>
2013-04-19 10:08:08 -0400

    add /weather/zipcode using geocoder and forecast_io

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
+require 'geocoder'
+require 'forecast_io'
+
+Forecast::IO.api_key = ENV['FORECAST_KEY']
+
 def roll
   ['&#9856;','&#9857;','&#9858;','&#9859;','&#9860;','&#9861;'][rand(0..5)]
 end
@@ -17,4 +22,14 @@ end
 get '/dice/:n' do
   content_type 'text/html'
   params[:n].to_i.times.map { roll }
+end
+
+get '/weather/:z' do
+  s = Geocoder.search(params[:z])
+  f = Forecast::IO.forecast(s[0].latitude.to_s, s[0].longitude.to_s)
+  d = f.daily.data[0]
+
+  # not happy with this or sure that it's right
+
+  "#{d.temperatureMin.to_s}&deg; - #{d.temperatureMax.to_s}&deg;"
 end
