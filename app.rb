e4ecc9b25583aa9cd7741398f3f1af94c8995821
Casey Olson <casey@bookcasey.com>
2013-04-19 13:35:30 -0400

    refactor content_type to before, add coat advice

--- app.rb
+++ app.rb
@@ -9,27 +9,35 @@ def roll
   ['&#9856;','&#9857;','&#9858;','&#9859;','&#9860;','&#9861;'][rand(0..5)]
 end
 
+before do
+  content_type "text/html"
+end
+
 get '/' do
-  content_type 'text/html'
   "Hi! Try /die or /dice/10"
 end
 
 get '/die' do
-  content_type 'text/html'
   roll
 end
 
 get '/dice/:n' do
-  content_type 'text/html'
   params[:n].to_i.times.map { roll }
 end
 
 get '/weather/:z' do
   s = Geocoder.search(params[:z])
   f = Forecast::IO.forecast(s[0].latitude.to_s, s[0].longitude.to_s)
-  d = f.daily.data[0]
+  d =  f.daily.data[0]
+  @t =  (d.temperatureMax + d.temperatureMin)/2
 
-  # not happy with this or sure that it's right
+  def coat
+    if @t <= 50
+      "Bring a light coat."
+    elsif @t <= 30
+      "Bring a coat and a scarf."
+    end
+  end
 
-  "#{d.temperatureMin.to_s}&deg; - #{d.temperatureMax.to_s}&deg;"
+  "#{f.hourly.summary} #{coat}" 
 end
